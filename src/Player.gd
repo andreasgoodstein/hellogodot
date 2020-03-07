5edd27f0c389fd97ef1d2faf8aa0f528b4597cd1
Andreas Goodstein <andreasgoodstein@gmail.com>
2020-03-07 13:38:22 +0700

    add labels

--- src/Player.gd
+++ src/Player.gd
@@ -1,10 +1,11 @@
 extends Sprite
 
 var move
+var moved_counter
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	moved_counter = get_parent().get_node('MovedCounter')	
 
 func _process(_delta: float) -> void:
 	var viewport_size = get_viewport().size
@@ -13,15 +14,19 @@ func _process(_delta: float) -> void:
 		'up':
 			if (position.y > 49):
 				move_local_y(-10)
+				increment_moved_count()
 		'right':
 			if (position.x < viewport_size.x - 49):
 				move_local_x(10)
+				increment_moved_count()
 		'down':
 			if (position.y < viewport_size.y - 49):
 				move_local_y(10)
+				increment_moved_count()
 		'left':
 			if (position.x > 49):
 				move_local_x(-10)
+				increment_moved_count()
 
 func _input(event: InputEvent) -> void:
 	move = ''
@@ -33,4 +38,9 @@ func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("ui_down")):
 		move = 'down'
 	if (event.is_action_pressed("ui_left")):
-		move = 'left'
+		move = 'left'	
+
+func increment_moved_count() -> void:	
+	var new_count = 'SP: ' + str(int(moved_counter.text) + 10)
+	moved_counter.clear()
+	moved_counter.add_text(new_count)
