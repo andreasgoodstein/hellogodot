56ea84cb8eab80ff971038f2decbb760c98746e9
Andreas Goodstein <andreasgoodstein@gmail.com>
2020-03-07 13:44:12 +0700

    fix input bug

--- src/Player.gd
+++ src/Player.gd
@@ -9,36 +9,26 @@ func _ready():
 
 func _process(_delta: float) -> void:
 	var viewport_size = get_viewport().size
-		
-	match move:
-		'up':
-			if (position.y > 49):
-				move_local_y(-10)
-				increment_moved_count()
-		'right':
-			if (position.x < viewport_size.x - 49):
-				move_local_x(10)
-				increment_moved_count()
-		'down':
-			if (position.y < viewport_size.y - 49):
-				move_local_y(10)
-				increment_moved_count()
-		'left':
-			if (position.x > 49):
-				move_local_x(-10)
-				increment_moved_count()
 
-func _input(event: InputEvent) -> void:
-	move = ''
-	
-	if (event.is_action_pressed("ui_up")):
-		move = 'up'
-	if (event.is_action_pressed("ui_right")):
-		move = 'right'
-	if (event.is_action_pressed("ui_down")):
-		move = 'down'
-	if (event.is_action_pressed("ui_left")):
-		move = 'left'	
+	if (Input.is_action_pressed("ui_up")):
+		if (position.y > 49):
+			move_local_y(-10)
+			increment_moved_count()
+
+	if (Input.is_action_pressed("ui_right")):
+		if (position.x < viewport_size.x - 49):
+			move_local_x(10)
+			increment_moved_count()
+
+	if (Input.is_action_pressed("ui_down")):
+		if (position.y < viewport_size.y - 49):
+			move_local_y(10)
+			increment_moved_count()
+
+	if (Input.is_action_pressed("ui_left")):
+		if (position.x > 49):
+			move_local_x(-10)
+			increment_moved_count()
 
 func increment_moved_count() -> void:	
 	var new_count = 'SP: ' + str(int(moved_counter.text) + 10)
